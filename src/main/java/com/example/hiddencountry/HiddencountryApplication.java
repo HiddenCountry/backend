9c75e38583ab41f69a0d3e14c80db1d0be6f4805
heeyun817 <vmffpfm79@naver.com>
2025-07-05 14:13:52 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/hiddencountry/HiddencountryApplication.java
@@ -0,0 +1,13 @@
+package com.example.hiddencountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddencountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiddencountryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hiddencountry/HiddencountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiddencountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddencountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
