2d48e6afef504917550d8e4873c41565d50b5fd1
RhoSeungA <sskclara@naver.com>
2025-09-18 10:12:38 +0900

    feat: JwtFilter 선택적 인증 모드 추가

--- src/main/java/com/example/hiddencountry/global/util/SecurityConstants.java
+++ src/main/java/com/example/hiddencountry/global/util/SecurityConstants.java
@@ -16,8 +16,12 @@ public class SecurityConstants {
             "/place",
             "/swagger-ui.html",
             "/swagger-ui/**",
-            "/v3/api-docs/**",
-            "/places",
-            "/places/map"
+            "/v3/api-docs/**"
+    );
+
+    /// 토큰이 있을 경우 인증 절차 진행하지만, 없을 경우에도 예외를 발생시키지는 않는 GET 메서드 URL
+    public static final List<String> GET__METHOD_ALLOW_URLS = List.of(
+        "/places",
+        "/places/map"
     );
 }
