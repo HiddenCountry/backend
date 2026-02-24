180ea62c8778224c6944822f618436be8566fa18
heeyun817 <vmffpfm79@naver.com>
2026-02-24 13:24:10 +0900

    refactor: cors 설정 변경 #61
    기존에 MVC레벨의 cors설정을 spring security레벨의 cors로 통합하여 변경함

--- src/main/java/com/example/hiddencountry/global/config/WebMvcConfig.java
+++ src/main/java/com/example/hiddencountry/global/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ import com.example.hiddencountry.global.handler.HiddenCountryUserArgumentHandler
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,19 +12,7 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final long MAX_AGE_SECS = 3600;
     private final HiddenCountryUserArgumentHandler hiddenCountryUserArgumentHandler;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://hiddencountry.vercel.app", "https://hiddencountry.store")
-                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
-    }
-
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
