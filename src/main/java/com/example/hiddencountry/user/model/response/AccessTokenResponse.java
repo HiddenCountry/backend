aab6e67cfc9eed9ef236032741b3d5e4d5e44fb8
heeyun817 <vmffpfm79@naver.com>
2026-02-27 13:33:06 +0900

    fix: '' 빌드 오류 제거

--- src/main/java/com/example/hiddencountry/user/model/response/AccessTokenResponse.java
+++ src/main/java/com/example/hiddencountry/user/model/response/AccessTokenResponse.java
@@ -3,7 +3,7 @@ package com.example.hiddencountry.user.model.response;
 import jakarta.validation.constraints.NotEmpty;
 
 public record AccessTokenResponse(
-    `@NotEmpty` String accessToken,
+    @NotEmpty String accessToken,
     boolean isFirstLogin
 ) {
 }
