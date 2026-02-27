0ec9b8278648544adacb425261e2ee4f5b27ccc4
Heeyun <90364739+heeyun817@users.noreply.github.com>
2026-02-27 13:27:58 +0900

    fix: 중복된 상수 선언 제거

--- src/main/java/com/example/hiddencountry/global/util/AuthCookieFactory.java
+++ src/main/java/com/example/hiddencountry/global/util/AuthCookieFactory.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 public class AuthCookieFactory {
 
     private static final String COOKIE_NAME = "refresh_token";
-    private static final String COOKIE_NAME = "refresh_token";
     private static final String COOKIE_PATH = "/auth";
     private static final String SAME_SITE = "None";
     private static final String SAME_SITE = "None";
